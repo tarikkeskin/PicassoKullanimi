e1f615945fe7b3ff0c4cacf004a6cdc701b5d566
tarikkeskin <tarik.keskin007@gmail.com>
2022-05-10 21:12:40 +0300

    Picasso

--- app/src/main/java/com/trigoo/picassokullanimi/MainActivity.java
+++ app/src/main/java/com/trigoo/picassokullanimi/MainActivity.java
@@ -4,11 +4,34 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.squareup.picasso.Picasso;
+import com.trigoo.picassokullanimi.databinding.ActivityMainBinding;
 
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding tasarim;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        tasarim = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(tasarim.getRoot());
+
+        resimGoster("ayran.png");
+
+        //get permission because it is not https  to do that go manifest.xml
+        //http://kasimadalan.pe.hu/yemekler/resimler/kofte.png
+
+        tasarim.buttonResim1.setOnClickListener(view -> {
+            resimGoster("kofte.png");
+
+        });
+
+        tasarim.buttonResim2.setOnClickListener(view -> {
+            resimGoster("kadayif.png");
+        });
+    }
+
+    public void resimGoster(String resimAdi){
+        String url = "http://kasimadalan.pe.hu/yemekler/resimler/"+resimAdi;
+        Picasso.get().load(url).into(tasarim.imageView);
     }
 }
